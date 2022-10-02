b9d4e55f486d9bae9a4eb5e95a28e512221e92c8
chiesamghz <chiesamghz@gmail.com>
2022-10-02 23:57:17 +0700

    feat: add view for grading feature

---
+++ lib/pages/component/feed_daily_card.dart
@@ -0,0 +1,117 @@
+import 'package:flutter/material.dart';
+import 'package:fish/pages/pond/detail_feed_daily_page.dart';
+import 'package:fish/theme.dart';
+import 'package:get/get.dart';
+
+class FeedDailyCard extends StatelessWidget {
+  final String? day;
+  final num? feed;
+  final num? feedcount;
+
+  const FeedDailyCard({
+    Key? key,
+    this.day,
+    this.feed,
+    this.feedcount,
+  }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return GestureDetector(
+      onTap: () {
+        Get.to(() => DetailFeedDailyPage());
+      },
+      child: Container(
+        width: double.infinity,
+        margin: EdgeInsets.only(
+          top: defaultMargin,
+        ),
+        padding: EdgeInsets.all(20),
+        decoration: BoxDecoration(
+          borderRadius: BorderRadius.circular(20),
+          border: Border.all(color: primaryColor),
+          color: transparentColor,
+        ),
+        child: Column(
+          children: [
+            Row(
+              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+                Column(
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: [
+                    Text(
+                      "Hari",
+                      style: primaryTextStyle.copyWith(
+                        fontSize: 14,
+                        fontWeight: bold,
+                      ),
+                      overflow: TextOverflow.ellipsis,
+                      maxLines: 1,
+                    ),
+                    Text(
+                      this.day!,
+                      style: secondaryTextStyle.copyWith(
+                        fontSize: 14,
+                        fontWeight: medium,
+                      ),
+                      overflow: TextOverflow.ellipsis,
+                      maxLines: 1,
+                    ),
+                  ],
+                ),
+                Column(
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: [
+                    Text(
+                      "Pakan",
+                      style: primaryTextStyle.copyWith(
+                        fontSize: 14,
+                        fontWeight: bold,
+                      ),
+                      overflow: TextOverflow.ellipsis,
+                      maxLines: 1,
+                    ),
+                    Text(
+                      this.feed.toString() + " Kg",
+                      style: secondaryTextStyle.copyWith(
+                        fontSize: 14,
+                        fontWeight: medium,
+                      ),
+                      overflow: TextOverflow.ellipsis,
+                      maxLines: 1,
+                    ),
+                  ],
+                ),
+                Column(
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: [
+                    Text(
+                      "Ikan",
+                      style: primaryTextStyle.copyWith(
+                        fontSize: 14,
+                        fontWeight: bold,
+                      ),
+                      overflow: TextOverflow.ellipsis,
+                      maxLines: 1,
+                    ),
+                    Text(
+                      this.feedcount.toString() + " Kali",
+                      style: secondaryTextStyle.copyWith(
+                        fontSize: 14,
+                        fontWeight: medium,
+                      ),
+                      overflow: TextOverflow.ellipsis,
+                      maxLines: 1,
+                    ),
+                  ],
+                ),
+              ],
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
---
+++ lib/pages/pond/feed_entry_page.dart
@@ -0,0 +1,219 @@
+import 'dart:async';
+
+import 'package:fish/pages/pond/feed_entry_controller.dart';
+import 'package:flutter/material.dart';
+import 'package:fish/theme.dart';
+import 'package:flutter/services.dart';
+import 'package:get/get.dart';
+
+import 'material_controller.dart';
+
+class FeedEntryPage extends StatelessWidget {
+  const FeedEntryPage({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    final FeedEntryController controller = Get.put(FeedEntryController());
+    TextEditingController pondInputController = TextEditingController(text: '');
+    TextEditingController feedTypeController = TextEditingController(text: '');
+    TextEditingController feedDosisController = TextEditingController(text: '');
+
+    Widget pondInput() {
+      return Container(
+        margin: EdgeInsets.only(
+            top: defaultSpace, right: defaultMargin, left: defaultMargin),
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            Text(
+              'Kolam (meter)',
+              style: primaryTextStyle.copyWith(
+                fontSize: 16,
+                fontWeight: medium,
+              ),
+            ),
+            SizedBox(
+              height: 12,
+            ),
+            Container(
+              height: 50,
+              padding: EdgeInsets.symmetric(
+                horizontal: 16,
+              ),
+              decoration: BoxDecoration(
+                color: backgroundColor2,
+                borderRadius: BorderRadius.circular(12),
+              ),
+              child: Center(
+                child: Expanded(
+                  child: TextFormField(
+                    style: primaryTextStyle,
+                    inputFormatters: <TextInputFormatter>[
+                      FilteringTextInputFormatter.digitsOnly
+                    ],
+                    keyboardType: TextInputType.number,
+                    controller: pondInputController,
+                    decoration: InputDecoration.collapsed(
+                      hintText: 'ex: 2',
+                      hintStyle: subtitleTextStyle,
+                    ),
+                  ),
+                ),
+              ),
+            ),
+          ],
+        ),
+      );
+    }
+
+    Widget feedTypeInput() {
+      return Container(
+        margin: EdgeInsets.only(
+            top: defaultSpace, right: defaultMargin, left: defaultMargin),
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            Text(
+              'Jenis Pakan',
+              style: primaryTextStyle.copyWith(
+                fontSize: 16,
+                fontWeight: medium,
+              ),
+            ),
+            SizedBox(
+              height: 12,
+            ),
+            Container(
+              height: 50,
+              padding: EdgeInsets.symmetric(
+                horizontal: 16,
+              ),
+              decoration: BoxDecoration(
+                color: backgroundColor2,
+                borderRadius: BorderRadius.circular(12),
+              ),
+              child: Center(
+                child: Expanded(
+                  child: TextFormField(
+                    style: primaryTextStyle,
+                    inputFormatters: <TextInputFormatter>[
+                      FilteringTextInputFormatter.digitsOnly
+                    ],
+                    keyboardType: TextInputType.number,
+                    controller: feedTypeController,
+                    decoration: InputDecoration.collapsed(
+                      hintText: 'ex: Pelet',
+                      hintStyle: subtitleTextStyle,
+                    ),
+                  ),
+                ),
+              ),
+            ),
+          ],
+        ),
+      );
+    }
+
+    Widget feedDosisInput() {
+      return Container(
+        margin: EdgeInsets.only(
+            top: defaultSpace, right: defaultMargin, left: defaultMargin),
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            Text(
+              'DOsis Pakan (gram)',
+              style: primaryTextStyle.copyWith(
+                fontSize: 16,
+                fontWeight: medium,
+              ),
+            ),
+            SizedBox(
+              height: 12,
+            ),
+            Container(
+              height: 50,
+              padding: EdgeInsets.symmetric(
+                horizontal: 16,
+              ),
+              decoration: BoxDecoration(
+                color: backgroundColor2,
+                borderRadius: BorderRadius.circular(12),
+              ),
+              child: Center(
+                child: Expanded(
+                  child: TextFormField(
+                    style: primaryTextStyle,
+                    inputFormatters: <TextInputFormatter>[
+                      FilteringTextInputFormatter.digitsOnly
+                    ],
+                    keyboardType: TextInputType.number,
+                    controller: feedDosisController,
+                    decoration: InputDecoration.collapsed(
+                      hintText: 'ex: 20',
+                      hintStyle: subtitleTextStyle,
+                    ),
+                  ),
+                ),
+              ),
+            ),
+          ],
+        ),
+      );
+    }
+
+    Widget submitButton() {
+      return Container(
+        height: 50,
+        width: double.infinity,
+        margin: EdgeInsets.only(
+            top: defaultSpace * 3, right: defaultMargin, left: defaultMargin),
+        child: TextButton(
+          onPressed: () {},
+          style: TextButton.styleFrom(
+            backgroundColor: primaryColor,
+            shape: RoundedRectangleBorder(
+              borderRadius: BorderRadius.circular(12),
+            ),
+          ),
+          child: Text(
+            'Submit',
+            style: primaryTextStyle.copyWith(
+              fontSize: 16,
+              fontWeight: medium,
+            ),
+          ),
+        ),
+      );
+    }
+
+    return Obx(() {
+      if (controller.isLoading.value == false) {
+        return Scaffold(
+          appBar: AppBar(
+            backgroundColor: backgroundColor2,
+            title: const Text("Entry Pakan"),
+          ),
+          backgroundColor: backgroundColor1,
+          body: ListView(
+            children: [
+              pondInput(),
+              feedTypeInput(),
+              feedDosisInput(),
+              submitButton(),
+              SizedBox(
+                height: 8,
+              )
+            ],
+          ),
+        );
+      } else {
+        return Center(
+          child: CircularProgressIndicator(
+            color: secondaryColor,
+          ),
+        );
+      }
+    });
+  }
+}
